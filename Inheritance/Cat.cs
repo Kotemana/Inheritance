4f57b32e9bedb02f67af2d25d07df0fbab6510ab
Leonid Radkevich <laradkevich@gmail.com>
2021-10-20 21:56:17 +0300

    Add project files.

---
+++ Inheritance/Cat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Cat : Animal
+    {
+        public bool HasBalls { get; set; }
+        public override void SaySomething()
+        {
+            //string voice = HasBalls ? "meows" : "miiiiws";
+            string voice;
+            if (HasBalls)
+            {
+                voice = "meows";
+            }
+            else
+            {
+                voice = "miiiiws";
+            }
+            Console.WriteLine($"{Name} {voice}");
+        }
+    }
+}
---
+++ Inheritance/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var zoo = new List<Animal>();
+            zoo.Add(new Dog() { Name = "Barbos" });
+            zoo.Add(new Cat() { Name = "Ryzhik", HasBalls = true });
+            zoo.Add(new Cat() { Name = "Dexter", HasBalls = false });
+            zoo.Add(new Animal() { Name = "Cheburashka" });
+            zoo.Add(new Fish() { Name = "Yellow striped", IsEadible = true });
+            zoo.Add(new Fish() { Name = "Fugu", IsEadible = false });
+
+            foreach (var animal in zoo)
+            {
+                animal.SaySomething();
+                
+                if(animal is Cat cat)
+                {
+                    Console.WriteLine($"Cat is CAT: {cat.HasBalls}");
+                }
+                if(animal is Fish fishyFishy)
+                {
+                    if (!fishyFishy.IsEadible)
+                    {
+                        Console.WriteLine("You Died!");
+                    }
+                }
+            }
+        }
+    }
+}
